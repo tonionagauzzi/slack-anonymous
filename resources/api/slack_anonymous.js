8118683262c3e47c2197356dd765b868cf015c95
nagauzzi <kensaku.nagauchi@access-company.com>
2021-06-23 21:09:19 +0900

    Test

--- resources/api/slack_anonymous.js
+++ resources/api/slack_anonymous.js
@@ -1,12 +1,17 @@
 const { WebClient } = require('@slack/web-api');
+const { createEventAdapter } = require('@slack/events-api');
 const token = process.env.TOKEN;
 const web = new WebClient(token);
 const querystring = require('querystring');
 
-exports.handler = async (event) => {
+console.log(process.env.SLACK_SIGNING_SECRET);
+const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
+
+slackEvents.on('message', async (slackEvent) => {
+    console.log(slackEvent.channel);
     const token = process.env.SLACK_BOT_OAUTH_TOKEN;
-    const body = querystring.parse(event.body);
-    const channel = event.channel;
+    const body = querystring.parse(slackEvent.body);
+    const channel = slackEvent.channel;
     const result = await web.chat.postMessage({
         token: token,
         text: body.text,
@@ -16,4 +21,4 @@ exports.handler = async (event) => {
         statusCode: 200,
         body: 'Posted your message as anonymous.',
     };
-};
+});
