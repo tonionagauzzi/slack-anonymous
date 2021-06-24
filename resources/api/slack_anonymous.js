b9f713bba9e8662f39d741c4dc4018640c805834
nagauzzi <kensaku.nagauchi@access-company.com>
2021-06-24 10:15:21 +0900

    Support thread but not working.

--- resources/api/slack_anonymous.js
+++ resources/api/slack_anonymous.js
@@ -5,6 +5,18 @@ const querystring = require('querystring');
 
 exports.handler = async (event) => {
     const body = querystring.parse(event.body);
+    if (body.thread_ts) {
+        const result = await web.chat.postMessage({
+            token: process.env.SLACK_BOT_OAUTH_TOKEN,
+            thread_ts: body.thread_ts,
+            text: body.text,
+            channel: body.channel_id,
+        });
+        return {
+            statusCode: 200,
+            body: 'Posted your message in thread as anonymous.',
+        };
+    }
     const result = await web.chat.postMessage({
         token: process.env.SLACK_BOT_OAUTH_TOKEN,
         text: body.text,
