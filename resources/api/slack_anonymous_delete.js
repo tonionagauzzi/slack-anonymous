6fa2cefdb3e89fb89eda601b8e93abe3bcfcde50
nagauzzi <kensaku.nagauchi@access-company.com>
2021-06-28 13:47:19 +0900

    Fix /anonymous_delete using patterns

--- resources/api/slack_anonymous_delete.js
+++ resources/api/slack_anonymous_delete.js
@@ -6,9 +6,14 @@ const querystring = require('querystring');
 exports.handler = async (event) => {
     const body = querystring.parse(event.body);
     const text = body.text;
-    const ts_no_period = text.substr(text.indexOf('/p')+2, 16);
-    const channel = text.substr(text.indexOf('/C')+1, 11);
+    const patternSplitURL = /[\/]/;
+    const ts_no_period = text.split(patternSplitURL).filter(function(textSplitted) {
+        return textSplitted.charAt(0) == 'p';
+    })[0].substring(1);
     const ts = ts_no_period.substr(0, 10) + "." + ts_no_period.substr(10, 6);
+    const channel = text.split(patternSplitURL).filter(function(textSplitted) {
+        return textSplitted.charAt(0) == 'C';
+    })[0];
     console.log(`channel=${channel}, ts=${ts}`);
     const result = await web.chat.delete({
         token: process.env.SLACK_BOT_OAUTH_TOKEN,
